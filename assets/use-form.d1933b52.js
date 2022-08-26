8741b52a003bf169db3d9eabe5ab51c7eab66ab4
paladyne-io <99455791+paladyne-io@users.noreply.github.com>
2022-08-26 19:17:57 +0900

    Fixed Dummy app example 2

---
+++ assets/use-form.d1933b52.js
@@ -0,0 +1,1 @@
+import{c as u,h as o}from"./index.5480e6f4.js";const s={name:String};function m(n={}){return(t,e,r)=>{t[e](o("input",{class:"hidden"+(r||""),...n.value}))}}function c(n){return u(()=>n.name||n.for)}export{c as a,m as b,s as u};
---
+++ assets/use-quasar.0688c750.js
@@ -0,0 +1,1 @@
+import{a,a4 as r}from"./index.5480e6f4.js";function u(){return a(r)}export{u};
