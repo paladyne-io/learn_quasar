4f7d2ca5bed61135381644a813b76a0ce93e49ed
paladyne-io <99455791+paladyne-io@users.noreply.github.com>
2022-09-02 09:23:45 +0900

    Improved Rocket man app (demo)

---
+++ assets/use-form.0aec8d70.js
@@ -0,0 +1,1 @@
+import{c as u,h as o}from"./index.b4672b1f.js";const s={name:String};function m(n={}){return(t,e,r)=>{t[e](o("input",{class:"hidden"+(r||""),...n.value}))}}function c(n){return u(()=>n.name||n.for)}export{c as a,m as b,s as u};
