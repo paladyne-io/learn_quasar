e3042b6104f3e55e11e0b98daa991bb3b074fe24
paladyne-io <99455791+paladyne-io@users.noreply.github.com>
2022-10-03 18:00:45 +0900

    Updated Installation instructions

---
+++ assets/QSpace.db7b8e2c.js
@@ -0,0 +1,1 @@
+import{h as e,f as a}from"./index.3e78c1a9.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
--- assets/use-quasar.abc440a7.js
+++
@@ -1,1 +0,0 @@
-import{a,ah as r}from"./index.a1f601e0.js";function u(){return a(r)}export{u};
