4c4bd695702ee8ed9f0b498ae3e857ee6a5bcc69
paladyne-io <darren@paladyne.jp>
2022-11-09 07:44:49 +0900

    Minor updates

---
+++ assets/use-form.6f1bf1a0.js
@@ -0,0 +1,1 @@
+import{a as n,h as r}from"./index.d92ddc89.js";const o={name:String};function s(e){return n(()=>({type:"hidden",name:e.name,value:e.modelValue}))}function c(e={}){return(t,u,a)=>{t[u](r("input",{class:"hidden"+(a||""),...e.value}))}}function i(e){return n(()=>e.name||e.for)}export{c as a,i as b,s as c,o as u};
--- assets/use-quasar.0d1bcba3.js
+++
@@ -1,1 +0,0 @@
-import{a,ad as r}from"./index.4d31a4bc.js";function u(){return a(r)}export{u};
